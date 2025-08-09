5df94ea8124d8ff22bcf070dd800087b80ac4465
duxevy <pechenevskyvladislav@gmail.com>
2025-08-09 12:36:56 +0300

    docstring

--- src/masks.py
+++ src/masks.py
@@ -8,5 +8,7 @@ def get_mask_card_number(card_number: str) -> str:
 def get_mask_account(account_number: str) -> str:
     """
     Маскирует номер счёта, оставляя только последние 4 цифры.
+
+    Return: masked account number
     """
     return f"**{account_number[-4:]}"
